87b1acf8f0a1df0f70c8fdeb76906578430f4770
charliechang <elixirbb@gmail.com>
2021-01-12 18:08:51 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,19 @@ curl $(fwdproxy-config curl) -fLo ~/.local/share/nvim/site/autoload/plug.vim --c
 export https_proxy=http://fwdproxy:8080;~/nvim +PlugInstall +qall; unset https_proxy
 
 # cd to right folder
+SESSION_NAME="development"
+tmux new -d -s $SESSION_NAME
 if [[ "$TW_JOB_NAME" == "sigma" ]]; then 
   cd ~/si_sigma
+  tmux split-window -t $SESSION_NAME
+  tmux send-keys -t $SESSION_NAME:0.0 "vim" ENTER
+  tmux send-keys -t $SESSION_NAME:0.1 "haxlsh" ENTER
+  tmux new-window -t $SESSION_NAME
+  tmux send-keys -t $SESSION_NAME:1.0 "hg ssl" ENTER
 elif [[ "$TW_JOB_NAME" == "www" ]]; then 
   cd ~/www
+  tmux send-keys -t $SESSION_NAME:0.0 "vim" ENTER
+  tmux new-window -t $SESSION_NAME
+  tmux send-keys -t $SESSION_NAME:1.0 "hg ssl" ENTER
 fi
-exec bash #for staying at the folder
+exec tmux a -t $SESSION_NAME # #for staying at the folder
