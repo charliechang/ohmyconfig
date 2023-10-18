6bf0c921eb3057c7cf0e5a0f0c5149f1a9284e06
Charlie Chang <charliechang@Charlies-MacBook-Pro.local>
2023-10-18 15:33:17 +0800

    Update

---
+++ nvim/lua/plugins/nvim-telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-telescope/telescope.nvim", 
+  tag = "0.1.4",
+  dependencies = { 
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>")
+    vim.keymap.set("n", "<C-f>", "<cmd>Telescope grep_string<cr>")
+  end,
+}
