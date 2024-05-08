a781e78c871a02988816535d3e2727726533d442
Charlie Chang <charliechang@kkcompany.com>
2024-05-08 22:54:05 +0800

    genai

--- nvim/lua/plugins/nvim-genai.lua
+++ nvim/lua/plugins/nvim-genai.lua
@@ -7,11 +7,17 @@ return {
   config = function()
     local gen = require('gen')
 
-    gen.prompts['Enhance_Grammar_Spelling_Markdown'] = {
+    gen.prompts['01_Fix_Typo_Only'] = {
+      prompt = "Fix following text if there are any typos, just output the final text:\n$text",
+      replace = true,
+    }
+
+    gen.prompts['02_Enhance_Grammar_Spelling_Markdown'] = {
       prompt = "Modify the following text to improve grammar and spelling, just output the final text and try to keep original markdown format:\n$text",
       replace = true,
     }
 
-    vim.keymap.set('v', 'z=', ':Gen Enhance_Grammar_Spelling_Markdown<CR>')
+
+    vim.keymap.set('v', 'z=', ':Gen 01_Fix_Typo_Only<CR>')
   end,
 }
