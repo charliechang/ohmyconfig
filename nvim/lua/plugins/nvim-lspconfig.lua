d6d8ca910dc9b71c01b7074695686a119ada6bb7
Charlie Chang <elixirbb@gmail.com>
2025-12-17 00:04:10 +0800

    adjustment for my MBA

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,17 +1,16 @@
-return   {
+return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
-    local lspconfig = require("lspconfig")
-    local util = require "lspconfig/util"
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local capabilities = cmp_nvim_lsp.default_capabilities()
-    local opts = { noremap = true, silent = true }
-    local on_attach = function(client, bufnr)
-      opts.buffer = bufnr
+    -- Set default position encoding to silence the warning
+    capabilities.general = capabilities.general or {}
+    capabilities.general.positionEncodings = { "utf-16" }
 
+    local on_attach = function(client, bufnr)
       -- set keybinds
       vim.keymap.set("n", 'gi', require("telescope.builtin").lsp_implementations, { buffer = bufnr, desc = "[g]oto [i]mplementations" })
       vim.keymap.set("n", 'gr', require("telescope.builtin").lsp_references, { buffer = bufnr, desc = "[g]oto [r]eferences" })
@@ -19,12 +18,13 @@ return   {
       vim.keymap.set("n", 'ga', vim.lsp.buf.code_action, { buffer = bufnr, desc = "[g]oto [a]ctions" })
     end
 
-    lspconfig["gopls"].setup {
-      on_attach = on_attach,
+    -- Configure LSP servers using the new vim.lsp.config API
+    vim.lsp.config("gopls", {
       capabilities = capabilities,
+      on_attach = on_attach,
       cmd = {"gopls"},
       filetypes = { "go", "gomod", "gowork", "gotmpl" },
-      root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+      root_markers = {"go.work", "go.mod", ".git"},
       settings = {
         gopls = {
           completeUnimported = true,
@@ -34,12 +34,13 @@ return   {
           },
         },
       },
-    }
-    lspconfig["pyright"].setup({
+    })
+
+    vim.lsp.config("pyright", {
       capabilities = capabilities,
       on_attach = on_attach,
       filetypes = {"python"},
-      root_dir = util.root_pattern("requirements.txt",".git"),
+      root_markers = {"requirements.txt", ".git"},
       settings = {
         python = {
           pythonPath = vim.fn.exepath("python3") or vim.fn.exepath("python"),
@@ -47,48 +48,58 @@ return   {
       },
     })
 
-    lspconfig["terraformls"].setup{}
+    vim.lsp.config("terraformls", {
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
+    vim.lsp.config("bashls", {
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
+    vim.lsp.config("rust_analyzer", {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      filetypes = {"rust"},
+      root_markers = {"Cargo.toml"},
+    })
+
+    vim.lsp.config("ts_ls", {
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
+    vim.lsp.config("dockerls", {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      filetypes = {"dockerfile"},
+    })
+
+    vim.lsp.config("docker_compose_language_service", {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      filetypes = {"yaml"},
+    })
+
+    -- Enable all configured LSP servers
+    vim.lsp.enable({
+      "gopls",
+      "pyright",
+      "terraformls",
+      "bashls",
+      "rust_analyzer",
+      "ts_ls",
+      "dockerls",
+      "docker_compose_language_service",
+    })
+
+    -- Terraform format on save
     vim.api.nvim_create_autocmd({"BufWritePre"}, {
       pattern = {"*.tf", "*.tfvars"},
       callback = function()
         vim.lsp.buf.format()
       end,
     })
-
-    lspconfig["bashls"].setup{}
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'sh',
-      callback = function()
-        vim.lsp.start({
-          name = 'bash-language-server',
-          cmd = { 'bash-language-server', 'start' },
-        })
-      end,
-    })
-
-    lspconfig['rust_analyzer'].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = {"rust"},
-      root_dir = util.root_pattern("Cargo.toml"),
-    })
-
-    lspconfig['ts_ls'].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    lspconfig["dockerls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = {"dockerfile"},
-    })
-
-    lspconfig["docker_compose_language_service"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = {"yaml"},
-    })
-
   end,
 }
--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,3 +1,13 @@
 return {
-  "christoomey/vim-tmux-navigator"
+  "alexghergh/nvim-tmux-navigation",
+  config = function()
+    require("nvim-tmux-navigation").setup({
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+      }
+    })
+  end
 }
